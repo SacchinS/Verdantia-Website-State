51d7bbf486b2d1e8cb156f75ebe48b727ae849bf
SacchinS <sacchinsar30@gmail.com>
2024-04-14 14:15:33 -0700

    Updated units to vw for scalability

--- verdantia-website/src/app/components/landingContent.tsx
+++ verdantia-website/src/app/components/landingContent.tsx
@@ -1,3 +1,5 @@
+// components/LandingPage.tsx
+
 import Navbar from './navbar';
 
 interface LandingPageProps {
@@ -13,14 +15,14 @@ const LandingPage: React.FC<LandingPageProps> = ({ heading, subheading, buttonTe
     <div>
       <div className="absolute inset-0 w-screen h-screen bg-center bg-cover" style={{ backgroundImage: `url('/images/background-cropped.png')`, zIndex: -1, height: '85vh'}}></div>
       <Navbar />
-      <div className="container mx-auto py-3 px-6 text-white relative z-10 ml-20 mr-20"> {/* Set explicit left and right margins */}
-        <div className="text-3xl mb-4" style={{ fontFamily: 'Enriqueta', color: '#3E3E3E', fontSize: '100px', lineHeight: '1.2' }}> {/* Adjusted line height */}
+      <div className="container mx-auto py-3 px-6 text-white relative z-10 ml-20">
+        <div className="text-3xl mb-4" style={{ fontFamily: 'Enriqueta', color: '#3E3E3E', fontSize: '7vw', lineHeight: '8vw' }}>
           {headingPart1 && <div>{headingPart1}</div>}
           {headingPart2 && <div>{headingPart2}</div>}
         </div>
-        <div className="text-lg mb-8" style={{ fontFamily: 'Bellota Text', color: '#3E3E3E', fontSize: '30px', lineHeight: '2' }}>{subheading}</div>
+        <div className="text-lg mb-8" style={{ fontFamily: 'Bellota Text', color: '#3E3E3E', fontSize: '2.1vw', lineHeight: '3vw' }}>{subheading}</div>
         {buttonText && (
-          <button className="bg-gray-700 text-white font-semibold px-6 py-2 rounded hover:bg-gray-800 shadow-lg" style={{ fontFamily: 'Bellota Text', fontSize: 20, backgroundColor: '#3E3E3E', borderRadius: '25px', boxShadow: '0 2px 6px rgba(0, 0, 0, 0.5)' }}>
+          <button className="bg-gray-700 text-white font-semibold px-6 py-2 rounded hover:bg-gray-800 shadow-lg" style={{ fontFamily: 'Bellota Text', fontSize: '1.4vw', backgroundColor: '#3E3E3E', borderRadius: '25px', boxShadow: '0 4px 12px rgba(0, 0, 0, 0.7)' }}>
             {buttonText}
           </button>
         )}
--- verdantia-website/src/app/components/navbar.tsx
+++ verdantia-website/src/app/components/navbar.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link';
 const Navbar: React.FC = () => {
   return (
     <div>
-      <div className="bg-custom-green py-3 mt-5"></div> {/* Adjusted padding with mt-2 */}
+      <div className="bg-custom-green py-3 mt-2"></div>
       <div className="container mx-auto flex items-center justify-between py-4 px-6" style={{ fontFamily: 'Bellota Text' }}>
-        <div className="mx-20"> {/* Adjusted margin with mx-10 */}
-          <div className="text-black" style={{ fontSize: '30px' }}>
+        <div className="mx-20">
+          <div className="text-black" style={{ fontSize: '4vh' }}>
             VERDANTIA | Careers
           </div>
         </div>
-        <div className="flex items-center space-x-4 mx-20"> {/* Adjusted margin with mx-10 */}
+        <div className="flex items-center space-x-4 mx-20" style={{ fontSize: '3vh' }}>
           <NavLink href="/" text="Home" />
           <NavLink href="/benefits" text="Benefits" />
           <NavLink href="/jobs" text="Jobs" />
