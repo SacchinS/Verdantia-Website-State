0657e12c1a21d120298deb7f459a8ec74f892266
SacchinS <sacchinsar30@gmail.com>
2024-04-22 19:34:06 -0700

    jobs page heading

--- verdantia-website/src/app/jobs/page.tsx
+++ verdantia-website/src/app/jobs/page.tsx
@@ -1,12 +1,10 @@
 import SignUpPopUp from "@/app/components/signUpPopUp";
 import SignInPopUp from "@/app/components/signInPopUp";
+import BodyHeading from "../components/bodyHeading";
 export default function Jobs() {
     return (
       <main>
-        <h2>Jobs Page</h2>
-          <SignUpPopUp/>
-          <h2>break</h2>
-        <SignInPopUp/>
+        <BodyHeading>Jobs at Verdantia</BodyHeading>
       </main>
     )
   }
