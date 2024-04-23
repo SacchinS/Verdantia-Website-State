46d4780a270ac99e8e8647c50e5f5fd70c8d0756
vabhi2007 <70085064+vabhi2007@users.noreply.github.com>
2024-04-23 01:01:58 -0700

    broken code

--- verdantia-website/src/app/components/LocationFilter.tsx
+++ verdantia-website/src/app/components/LocationFilter.tsx
@@ -1,73 +1,74 @@
 // LocationFilter.tsx
 import React, { useState } from 'react';
+import {arrayUnion, doc, setDoc, updateDoc} from "firebase/firestore";
+import {db} from "@/app/firebase/config";
 
-interface LocationFilterProps {
-  onChange: (locations: string[], blocks: string[]) => void;
-}
 
-const LocationFilter: React.FC<LocationFilterProps> = ({ onChange }) => {
-  const [selectedLocation, setSelectedLocation] = useState<string | null>('');
-  const [selectedBlocks, setSelectedBlocks] = useState<string[]>([]);
 
-  const handleLocationChange = (location: string) => {
-    setSelectedLocation(location);
-    onChange([location], selectedBlocks);
-  };
+const LocationFilter: React.FC = async () => {
 
-  const handleBlockClick = (block: string) => {
-    const updatedBlocks = [...selectedBlocks];
-    const index = updatedBlocks.indexOf(block);
-    if (index === -1) {
-      updatedBlocks.push(block);
-    } else {
-      updatedBlocks.splice(index, 1);
-    }
-    setSelectedBlocks(updatedBlocks);
-    onChange([selectedLocation || ''], updatedBlocks);
-  };
+
+  const addPlaceFilter = async (place:string) => {
+    const ref = doc(db, "selectedFilters", "selectedPlace");
+    await updateDoc(ref, {
+      places: arrayUnion(place)
+    });
+  }
+
+  const addLocationFilter = async (location:string) => {
+    const ref = doc(db, "selectedFilters", "selectedLocation");
+    await updateDoc(ref, {
+      location: location
+    });
+  }
 
   return (
-    <div className="w-full mr-8">
-      <h3>Location</h3>
-      <div>
-        <select
-          className="w-full border border-gray-300 rounded p-2"
-          value={selectedLocation || ''}
-          onChange={(e) => handleLocationChange(e.target.value)}
-        >
-          <option value="">Select Location</option>
-          <option value="Seattle, WA">Seattle, WA</option>
-          <option value="London, UK">London, UK</option>
-          <option value="New York City, NY">New York City, NY</option>
-        </select>
+      <div className="w-full mr-8 flex flex-col">
+        <div className='mb-[1vw]' style={{fontFamily: "Bellota Text", fontSize: '1.5vw'}}>Location</div>
+        <div>
+          <select
+              className="shadow-md w-full border border-gray-300 rounded p-3 text-lg"
+              style={{fontFamily: "Bellota Text"}} // Adjust height and font size
+              value={selectedLocation || ''}
+              onChange={(e) => addLocationFilter(e.target.value)}
+          >
+            <option value="">Select Location</option>
+            <option value="Seattle, WA">Seattle, WA</option>
+            <option value="London, UK">London, UK</option>
+            <option value="New York City, NY">New York City, NY</option>
+          </select>
+        </div>
+        <div className="flex justify-center mt-4 gap-10 flex-grow">
+          <button
+              style={{fontFamily: "Bellota Text", fontSize: '1.5vw', width: '100%'}} // Set width to 100%
+              className={`shadow-md h-[4vw] px-4 py-2 border rounded ${
+                  selectedBlocks.includes('In Person') ? 'bg-button-green text-black' : ''
+              }`}
+              onClick={() => addPlaceFilter('In Person')}
+          >
+            In Person
+          </button>
+          <button
+              style={{fontFamily: "Bellota Text", fontSize: '1.5vw', width: '100%'}} // Set width to 100%
+              className={`shadow-md h-[4vw] px-4 py-2 border rounded ${
+                  selectedBlocks.includes('Hybrid') ? 'bg-button-green text-black' : ''
+              }`}
+              onClick={() => addPlaceFilter('Hybrid')}
+          >
+            Hybrid
+          </button>
+          <button
+              style={{fontFamily: "Bellota Text", fontSize: '1.5vw', width: '100%'}} // Set width to 100%
+              className={`shadow-md h-[4vw] px-4 py-2 border rounded ${
+                  selectedBlocks.includes('Remote') ? 'bg-button-green text-black' : ''
+              }`}
+              onClick={() => addPlaceFilter('Remote')}
+          >
+            Remote
+          </button>
+        </div>
       </div>
-      <div className="flex justify-center mt-4 gap-10"> 
-        <button
-          className={`px-4 py-2 border rounded ${
-            selectedBlocks.includes('In Person') ? 'bg-blue-500 text-white' : ''
-          }`}
-          onClick={() => handleBlockClick('In Person')}
-        >
-          In Person
-        </button>
-        <button
-          className={`px-4 py-2 border rounded ${
-            selectedBlocks.includes('Hybrid') ? 'bg-blue-500 text-white' : ''
-          }`}
-          onClick={() => handleBlockClick('Hybrid')}
-        >
-          Hybrid
-        </button>
-        <button
-          className={`px-4 py-2 border rounded ${
-            selectedBlocks.includes('Remote') ? 'bg-blue-500 text-white' : ''
-          }`}
-          onClick={() => handleBlockClick('Remote')}
-        >
-          Remote
-        </button>
-      </div>
-    </div>
+
   );
 };
 
--- verdantia-website/src/app/components/RoleFilter.tsx
+++ verdantia-website/src/app/components/RoleFilter.tsx
@@ -1,58 +1,78 @@
-import React, { useState } from 'react';
 
-interface RoleFilterProps {
-  onChange: (roles: string[]) => void;
-}
+import {
+  collection,
+  addDoc,
+  getDoc,
+  QuerySnapshot,
+  query,
+  onSnapshot,
+  deleteDoc,
+  doc, setDoc, updateDoc, arrayUnion, getDocFromCache,
+} from 'firebase/firestore';
+import {db} from '@/app/firebase/config'
 
-const RoleFilter: React.FC<RoleFilterProps> = ({ onChange }) => {
-  const [selectedRoles, setSelectedRoles] = useState<string[]>([]);
 
-  const handleRoleClick = (role: string) => {
-    const updatedRoles = [...selectedRoles];
-    const index = updatedRoles.indexOf(role);
-    if (index === -1) {
-      updatedRoles.push(role);
-    } else {
-      updatedRoles.splice(index, 1);
+
+
+const RoleFilter: React.FC = async () => {
+
+
+  const addRoleFilter = async (role:string) => {
+
+    const ref = doc(db, "selectedFilters", "selectedRoles");
+    await updateDoc(ref, {
+      roles: arrayUnion(role)
+    });
+  }
+
+  const getFilters = async () => {
+    const docRef = doc(db, "selectedFilters", "selectedRoles");
+    const docSnap = await getDoc(docRef);
+
+    const data = docSnap.data();
+    const dataArr: string[] = []
+    if (data){
+      const dataArr = Object.values(data);
     }
-    setSelectedRoles(updatedRoles);
-    onChange(updatedRoles);
-  };
+
+
+    console.log(data);
+    return dataArr;
+
+  }
 
   const roles = ['Coordinator', 'Manager', 'Engineer', 'Scientist', 'Analyst'];
 
   return (
-    <div>
-      <h3>Role</h3>
-      <div className="grid grid-cols-2 gap-4">
-        <div>
-          {roles.slice(0, 3).map(role => (
-            <button
-              key={role}
-              className={`block px-4 py-2 border rounded hover:bg-blue-500 hover:text-white mb-2 w-full ${
-                selectedRoles.includes(role) ? 'bg-blue-500 text-white' : ''
-              }`}
-              onClick={() => handleRoleClick(role)}
-            >
-              {role}
-            </button>
-          ))}
-        </div>
-        <div>
-          {roles.slice(3).map(role => (
-            <button
-              key={role}
-              className={`block px-4 py-2 border rounded hover:bg-blue-500 hover:text-white mb-2 w-full ${
-                selectedRoles.includes(role) ? 'bg-blue-500 text-white' : ''
-              }`}
-              onClick={() => handleRoleClick(role)}
-            >
-              {role}
-            </button>
-          ))}
+      <div>
+        <div className='mb-[1vw]' style={{fontFamily: "Bellota Text", fontSize: '1.5vw'}}>Role</div>
+        <div className="grid grid-cols-2 gap-[0vw]">
+          <div>
+            {roles.slice(0, 3).map(role => (
+                <button
+                    style={{fontFamily: "Bellota Text", fontSize: '1.5vw'}}
+                    key={role}
+                    className={"shadow-md block w-[15vw] h-[5vw] px-4 py-2 border rounded mb-[2vw]"}
+                    onClick={() => addRoleFilter(role)}
+                >
+                  {role}
+                </button>
+            ))}
+          </div>
+          <div>
+            {roles.slice(3).map(role => (
+                <button
+                    style={{fontFamily: "Bellota Text", fontSize: '1.5vw'}}
+                    key={role}
+                    className={"shadow-md block w-[15vw] h-[5vw] px-4 py-2 border rounded mb-[2vw]"}
+                    onClick={() => addRoleFilter(role)}
+                >
+                  {role}
+                </button>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
