6d60182bcb2207e28e7c8c02aae5076b556165fa
SacchinS <sacchinsar30@gmail.com>
2024-04-25 02:57:53 -0700

    Citations & footer

--- verdantia-website/src/app/citations/page.tsx
+++ verdantia-website/src/app/citations/page.tsx
@@ -9,13 +9,18 @@ import { motion } from "framer-motion";
 import { animate, useMotionValue } from "framer-motion";
 import { useState } from "react";
 import Carousel from '../components/Carousel';
+import Sources from '../../../public/images/Verdantia_Sources.svg';
+import Footer from "../components/Footer";
 
 const Citations = () => {
 
   return (
     <main>
-      <LandingContent heading="Citations" subheading="Testing ticker/carousel" />
-      
+      <LandingContent heading="Citations and\nReferences" subheading="" />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <img src={Sources.src} className='mt-[16vw] mb-[4vw] w-[50vw] h-auto'></img>
+      </div>
+      <Footer></Footer>
     </main>
   );
 };
