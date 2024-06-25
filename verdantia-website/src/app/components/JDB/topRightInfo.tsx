693f6089f5b7694b19fa90b1e6d04acd0dab4079
SacchinS <sacchinsar30@gmail.com>
2024-06-24 20:47:08 -0700

    tried to pprevent admin from applying and adding

--- verdantia-website/src/app/components/JDB/topRightInfo.tsx
+++ verdantia-website/src/app/components/JDB/topRightInfo.tsx
@@ -1,5 +1,10 @@
 // topRightInfo.tsx
-import React from 'react';
+import { db } from '@/app/firebase/config';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/app/firebase/config';
+import { useRouter } from 'next/navigation';
+import { collection, doc, onSnapshot, updateDoc, arrayUnion, setDoc, getDoc, arrayRemove } from 'firebase/firestore';
 
 interface TopRightInfoProps {
     onAddToList: () => void; // Add a prop to handle adding job to list
@@ -8,9 +13,35 @@ interface TopRightInfoProps {
     applyButtonText: string;
 }
 
+
 const TopRightInfo: React.FC<TopRightInfoProps> = ({ onAddToList, onApply, listButtonText, applyButtonText }) => {
+    const [user] = useAuthState(auth);
+    const router = useRouter();
+    let showButton = true;
+
+    useEffect(() => {
+        if (user) {
+            const userRef = doc(db, 'users', user.uid);
+            const unsubscribe = onSnapshot(userRef, (docSnapshot) => {
+                if (docSnapshot.exists()) {
+                    const data = docSnapshot.data();
+                    if (data.admin == true) {
+                        showButton = false;
+                    }
+                    else {
+                        showButton = true;
+                    }
+                }
+            });
+        }
+        else {
+            showButton = true;
+        }
+    }, [user, router]);
+
     return (
         <div className="inline-flex gap-[1vw] items-stretch">
+            {showButton && (
             <button 
                 className="flex-grow text-[0.75vw] rounded-[0.8vh] border border-[#b2b2b2] shadow-[0_0.3vh_0.3vh_0_rgba(0,0,0,0.25)] bg-opacity-100 bg-[#5b8c69] px-[1.5vw] py-[0.5vh] text-[#FFFFFF]" 
                 style={{ fontFamily: 'Montserrat' }}
@@ -18,7 +49,9 @@ const TopRightInfo: React.FC<TopRightInfoProps> = ({ onAddToList, onApply, listB
             >
                 {applyButtonText}
             </button>
+            )}
 
+            {showButton && (
             <button 
                 className="flex-grow text-[0.75vw] bg-white rounded-[0.8vh] border border-[#b2b2b2] shadow-[0_0.3vh_0.3vh_0_rgba(0,0,0,0.25)] px-[1.5vw] py-[0.5vh]" 
                 style={{ fontFamily: 'Montserrat' }}
@@ -26,6 +59,8 @@ const TopRightInfo: React.FC<TopRightInfoProps> = ({ onAddToList, onApply, listB
             >
                 {listButtonText}
             </button>
+            )}
+            
         </div>
     );
 };
