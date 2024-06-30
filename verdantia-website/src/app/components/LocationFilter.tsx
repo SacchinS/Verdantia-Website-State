8d00ed20d014f270cc9ed746f021a7fe6ade722c
DarkDestroyer976 <39469285+DarkDestroyer976@users.noreply.github.com>
2024-06-30 08:28:03 -0700

    fixed

--- verdantia-website/src/app/components/LocationFilter.tsx
+++ verdantia-website/src/app/components/LocationFilter.tsx
@@ -48,7 +48,7 @@ const LocationFilter: React.FC = () => {
         <select
           className="shadow-md w-full border border-gray-300 rounded p-3 text-[1.5vw]"
           style={{fontFamily: "Bellota Text"}}
-          value={selectedLocation}
+          value={selectedLocation!}
           onChange={(e) => updateLocationFilter(e.target.value)}
         >
           <option value="">All</option>
