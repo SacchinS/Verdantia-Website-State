fdb3085993ddf904d95f0412b42a47201f432f7b
s-ssaravanan <sacchinsar30@gmail.com>
2025-02-05 17:25:33 -0800

    Fixed child props problem

--- verdantia-website/src/app/components/bodyHeading.tsx
+++ verdantia-website/src/app/components/bodyHeading.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode;
+    children?: ReactNode;
     marginTop?: string;
     marginBottom?: string;
     centerAligned?: boolean; // Add centerAligned prop
